998b64c13eb586240cb7bdb89933ac9e05a734ee
halilcn <halilc.2001@gmail.com>
2022-06-20 17:44:56 +0300

    finish button test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,28 +1,64 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 import Button from './Button'
 
-//todo:tests
-
 describe('Button', () => {
-  const loadingElement = 'loading'
-  const contentElement = 'content'
+  const element = {
+    button: 'button',
+    loading: 'loading',
+    content: 'content',
+  }
+
+  const testClass = 'test-class-name'
 
   test('should be render without crashing', () => {
     render(<Button>Test Text Button</Button>)
   })
 
-  test('loading content should"t be null when loading prop is true', async () => {
-    render(<Button loading>Test Text Button</Button>)
+  describe('loading content', () => {
+    test('should"t be null when loading prop is true', async () => {
+      render(<Button loading>Test Text Button</Button>)
 
-    expect(screen.queryByTestId(contentElement)).toBeNull()
-    expect(screen.getByTestId(loadingElement)).toBeTruthy()
+      expect(screen.queryByTestId(element.content)).toBeNull()
+      expect(screen.getByTestId(element.loading)).toBeTruthy()
+    })
+
+    test('should be null when loading prop is false', async () => {
+      render(<Button>Test Text Button</Button>)
+
+      expect(screen.queryByTestId(element.loading)).toBeNull()
+      expect(screen.getByTestId(element.content)).toBeTruthy()
+    })
   })
 
-  test('loading content should be null when loading prop is false', async () => {
-    render(<Button>Test Text Button</Button>)
+  describe('button click function', () => {
+    test('should be called when button is clicked', async () => {
+      const handleClick = jest.fn()
+      render(<Button onClick={handleClick}>Test Text Button</Button>)
 
-    expect(screen.queryByTestId(loadingElement)).toBeNull()
-    expect(screen.getByTestId(contentElement)).toBeTruthy()
+      fireEvent.click(screen.getByTestId(element.button))
+
+      expect(handleClick).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('class of the button should', () => {
+    test('contain the added class name', async () => {
+      render(<Button className={testClass}>Test Text Button</Button>)
+
+      expect(screen.getByTestId(element.button).classList.contains(testClass)).toBeTruthy()
+    })
+
+    test('contain opacity class when disabled prop is true', async () => {
+      render(<Button disabled>Test Text Button</Button>)
+
+      expect(screen.getByTestId(element.button).classList.contains('opacity-50')).toBeTruthy()
+    })
+
+    test('contain opacity class when loading prop is true', async () => {
+      render(<Button loading>Test Text Button</Button>)
+
+      expect(screen.getByTestId(element.button).classList.contains('opacity-50')).toBeTruthy()
+    })
   })
 })
