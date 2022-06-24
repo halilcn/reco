3d97e33be8ca7d29ca48f8d4be66b8e6f6e1897b
halilcn <halilc.2001@gmail.com>
2022-06-24 11:24:00 +0300

    finish checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
+import { useState } from 'react'
+import { FaBeer } from 'react-icons/fa'
+
 import './App.css'
 import { Deneme } from './components'
 import Button from './components/Button/Button'
 import Checkbox from './components/Checkbox/Checkbox'
 
 function App() {
+  const [testCheck, setTestCheck] = useState(true)
+
   return (
     <div className="flex">
       <div className="mr-6">
         <Button className="bg-red-700">test button</Button>
       </div>
       <Checkbox
-        disabled
+        className=""
+        checked={testCheck}
         onChange={() => {
-          console.log('selam 1')
+          setTestCheck(!testCheck)
         }}
         id="test">
-        test check
+        tess deneme
       </Checkbox>
     </div>
   )
--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,31 +1,40 @@
 import classNames from 'classnames'
 import React from 'react'
 
+import convertClassesToImportant from '../../utils/convertClassesToImportant'
+
 interface IProps extends React.ComponentProps<'input'> {
   id: string
   children?: any
   disabled?: boolean
-  checked?: boolean
   loading?: boolean
 }
 
 const Checkbox: React.FC<IProps> = props => {
-  const { id, children, checked, loading, disabled, ...componentProps } = props
+  const { id, className, children, loading, disabled, ...componentProps } = props
 
-  //todo:dynamic checked?
-  //todo: defaultChecked ?
-  //todo: only icon ?
-  //todo: loading ?
+  const checkboxContainerClass = classNames(
+    'relative flex items-center text-gray-500 text-sm font-light',
+    convertClassesToImportant(className),
+    {
+      'pointer-events-none': disabled,
+      'pointer-events-none cursor-wait opacity-80': loading,
+    }
+  )
 
-  const checkboxContainerClass = classNames('relative flex items-center')
+  const checkboxClass = classNames('w-5 h-5 cursor-pointer checked:accent-sky-300')
 
-  const checkboxClass = classNames('w-5 h-5 cursor-pointer')
-
-  const checkboxLabelClass = classNames('ml-2 cursor-pointer text-sm font-light text-gray-500')
+  const checkboxLabelClass = classNames('ml-2 cursor-pointer')
 
   return (
     <div className={checkboxContainerClass}>
-      <input id={id} type="checkbox" className={checkboxClass} {...componentProps} />
+      <input
+        type="checkbox"
+        id={id}
+        className={checkboxClass}
+        disabled={disabled}
+        {...componentProps}
+      />
       {children && (
         <label htmlFor={id} className={checkboxLabelClass}>
           {children}
