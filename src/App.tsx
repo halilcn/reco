4e35d1e9b015104d0c495c05ff39c332b5d13922
halilcn <halilc.2001@gmail.com>
2022-07-04 23:05:06 +0300

    finish table component

--- src/App.tsx
+++ src/App.tsx
@@ -68,14 +68,16 @@ function App() {
       <div className="w-1/2">
         <Table
           rows={[
-            { field: 'price', headerName: 'Price' },
-            { field: 'title', headerName: 'Title' },
-            { field: 'description', headerName: 'Description' },
+            { field: 'price', headerName: 'Price', type: 'number', sortingActive: false },
+            { field: 'title', headerName: 'Title', type: 'string' },
+            { field: 'date', headerName: 'Date', type: 'date' },
           ]}
           columns={[
-            { price: 10, title: 'test asd', description: 'adsas' },
-            { price: 20, title: 'cxvxcv', description: 'twer' },
-            { price: 2, title: 'fdtdsdsest asd', description: 'yere' },
+            { price: 10, title: 'test asd', date: '11-12-2020' },
+            { price: 20, title: 'cxvxcv', date: '09-12-2020' },
+            { price: 2, title: 'fdtdsdsest asd', date: '01-12-2020' },
+            { price: 40, title: 'aa', date: '01-12-2025' },
+            { price: 42, title: 'b', date: '01-02-2025' },
           ]}
         />
       </div>
--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,60 +1,125 @@
 import classNames from 'classnames'
+import dayjs from 'dayjs'
 import React, { useEffect, useState } from 'react'
-import { FaSortUp } from 'react-icons/fa'
+import { TiArrowSortedDown, TiArrowSortedUp } from 'react-icons/ti'
 
 import convertClassesToImportant from '../../utils/convertClassesToImportant'
 
-export interface IRows {
+export interface IRow {
   field: string
   headerName: string
+  type: 'string' | 'number' | 'date'
+  sortingActive?: boolean
 }
 
 export interface IProps extends React.ComponentProps<'div'> {
-  rows: IRows[]
+  rows: IRow[]
   columns: any[]
+  rowsStyle?: string
+  columnsStyle?: string
 }
 
-enum SortType {
-  ASC = 'asc',
-  DESC = 'desc',
+export interface ActiveSort {
+  reverse: boolean
+  field: string
 }
-
-//todo:row ve title custom style?
 
 const Select: React.FC<IProps> = props => {
-  const { className, ...componentProps } = props
+  const { className, rows, columns, rowsStyle, columnsStyle, ...componentProps } = props
 
   const [tableColumns, setTableColumns] = useState<any[]>([])
+  const [rowFields, setRowFields] = useState<string[]>()
+  const [activeSorting, setActiveSorting] = useState<ActiveSort>({ reverse: false, field: '' })
 
   useEffect(() => {
-    setTableColumns([
-      { test: 1, test1: 2, test3: 3 },
-      { test: 5, test1: 2, test3: 3 },
-      { test: 3, test1: 2, test3: 3 },
-    ])
+    setTableColumns(columns)
+    setRowFields(rows.map(row => row.field))
   }, [])
 
-  const sortColum = (field: string, sortType: SortType) => {
-    alert(sortType)
+  const sortColum = (field: string) => {
+    if (findRowByField(field)?.sortingActive === false) return
+
+    const isReserve = activeSorting.field === field ? activeSorting.reverse : false
+
+    if (findRowByField(field)?.type === 'number') sortingByNumber(field, isReserve)
+    if (findRowByField(field)?.type === 'date') sortingByDate(field, isReserve)
+    if (findRowByField(field)?.type === 'string') sortingByString(field, isReserve)
+
+    setActiveSorting({ reverse: !activeSorting.reverse, field })
   }
 
+  const findRowByField = (field: string) => {
+    return rows.find(row => row.field === field)
+  }
+
+  const sortingByNumber = (field: string, reserve: boolean) => {
+    const sortedColumns = columns.sort(function (a: any, b: any) {
+      if (reserve) return parseInt(a[field]) > parseInt(b[field]) ? 1 : -1
+
+      return parseInt(a[field]) < parseInt(b[field]) ? 1 : -1
+    })
+
+    setTableColumns(sortedColumns)
+  }
+
+  const sortingByDate = (field: string, reserve: boolean) => {
+    const sortedColumns = columns.sort(function (a: any, b: any) {
+      if (reserve) return dayjs(a[field]).isAfter(b[field]) ? 1 : -1
+
+      return dayjs(a[field]).isBefore(b[field]) ? 1 : -1
+    })
+
+    setTableColumns(sortedColumns)
+  }
+
+  const sortingByString = (field: string, reserve: boolean) => {
+    const sortedColumns = columns.sort(function (a: any, b: any) {
+      return reserve ? a[field].localeCompare(b[field]) : b[field].localeCompare(a[field])
+    })
+
+    setTableColumns(sortedColumns)
+  }
+
+  const tableContainerClass = classNames('w-full border-2 rounded shadow-sm')
+
+  const rowContainerClass = classNames('flex justify-around border-b p-4 text-sm text-gray-800')
+
+  const rowItemClass = (row: IRow) => {
+    return classNames(
+      'flex items-center w-full cursor-pointer',
+      convertClassesToImportant(rowsStyle),
+      {
+        'cursor-pointer': row.sortingActive !== false,
+        'text-sky-500': activeSorting.field === row.field,
+      }
+    )
+  }
+
+  const columClass = classNames(
+    'flex justify-around p-4 border-b font-light text-sm text-gray-600 even:bg-gray-50',
+    convertClassesToImportant(columnsStyle)
+  )
+
   return (
-    <div className="w-full border-2 rounded shadow-sm">
-      <div className="flex justify-around border-b p-4 text-sm text-gray-800">
-        <div
-          onClick={() => sortColum('field_name', SortType.ASC)}
-          className="flex items-center w-full cursor-pointer text-sky-500">
-          Test asda s 1
-          <FaSortUp className="ml-3" />
-        </div>
-        <div className=" w-full">Test asd as1</div>
-        <div className=" w-full">Testds 1</div>
+    <div className={tableContainerClass} {...componentProps}>
+      <div className={rowContainerClass}>
+        {rows.map((row, key) => (
+          <div key={key} onClick={() => sortColum(row.field)} className={rowItemClass(row)}>
+            {row.headerName}
+            {row.sortingActive !== false &&
+              (activeSorting.reverse ? (
+                <TiArrowSortedDown className="ml-3" />
+              ) : (
+                <TiArrowSortedUp className="ml-3" />
+              ))}
+          </div>
+        ))}
       </div>
-      {tableColumns.map(column => (
-        <div className="flex justify-around p-4 border-b font-light text-sm	text-gray-600">
-          <div className=" w-full">{column.test}</div>
-          <div className=" w-full">{column.test1}</div>
-          <div className=" w-full">{column.test3}</div>
+      {tableColumns.map((column, key) => (
+        <div key={key} className={columClass}>
+          {rowFields?.map(rowField => (
+            <div className="w-full">{column[rowField]}</div>
+          ))}
         </div>
       ))}
     </div>
