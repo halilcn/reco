c6ef283af4ca0a045d384891be7463cc46d49a70
halilcn <halilc.2001@gmail.com>
2022-07-26 13:12:19 +0300

    first publish

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import React from 'react'
+export * from './components'
+
+/*import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
@@ -15,3 +17,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+*/
